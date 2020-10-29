712fefb6a056c69529e65a4f289c8348a5856008
DavisHenckel <davishenckel@hotmail.com>
2020-10-29 15:24:13 -0700

    Completed all but uniqueness for boats

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 @app.route('/')
 def index():
-    return "Please navigate to /boats"\
+    return "Please navigate to /boats"
 
+jsonErrorStringBoatID = '{ "Error":"No boat with this boat_id exists"}'
 jsonErrorStringArgs = '{ "Error":"The request object is missing at least one of the required attributes"}'
+jsonOnlyAcceptJson = '{ "Error":"This method only accepts JSON"}'
+jsonTypeNotSupported = '{ "Error": "This type is not supported, must be JSON or HTML"}'
+jsonInvalidRequest= '{ "Error": "IDs are immutable"}'
+jsonCannotDelAll = '{ "Error": "Cannot delete or edit the list of all boats"}'
 
 #assigns the self variable
 def create_self(item):
@@ -23,12 +28,25 @@ def create_self(item):
     item["self"] = selfURL
     return item
 
-@app.route('/boats', methods=['POST','GET'])
+def create_self_second(item):
+    item["self"] = request.base_url
+    return item
+
+#takes in list of results and returns a bool if key exists or not.
+def check_ID_existence(results, id):
+    for object in results:
+        if id == 'null':
+            break
+        if object.id == int(id):
+            return True #id is in
+    return False
+
+@app.route('/boats', methods=['POST','GET', 'PUT', 'DELETE'])
 def boats_get_or_post():
     if request.method == 'POST':
         content = request.get_json()
-        if (not(content)):
-            return('This method only accepts JSON', 415)
+        if (not(content)): #if could not get the content in json format
+            return(jsonOnlyAcceptJson, 415)
         if "name" not in content or "type" not in content or "length" not in content:
             return (jsonErrorStringArgs, 400)
         new_boat = datastore.entity.Entity(key=client.key(constants.boats))
@@ -42,11 +60,105 @@ def boats_get_or_post():
     elif request.method == 'GET':
         query = client.query(kind=constants.boats)
         results = list(query.fetch())
-        res = make_response(json2html.convert(json = json.dumps(results)))
-        res.headers.set('Content-Type', 'text/html')
-        res.status_code = 200
-        return res
+        if 'application/json' in request.accept_mimetypes:
+            query = client.query(kind=constants.boats)
+            results = list(query.fetch())
+            for e in results:
+                e["id"] = e.key.id
+            jsonData = json.dumps(results)
+            return (jsonData, 200)
+        elif 'text/html' in request.accept_mimetypes:
+            res = make_response(json2html.convert(json = json.dumps(results)))
+            res.headers.set('Content-Type', 'text/html')
+            res.status_code = 200
+            return res
+        else:
+            return (jsonTypeNotSupported, 406)
+    
+    elif request.method == 'PUT' or request.method == 'DELETE':
+        return (jsonCannotDelAll, 405)
+    
+    else:
+        return 'Method not recogonized'
 
+@app.route('/boats/<id>', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
+def boats_get_delete_patch(id):
+    if request.method == 'DELETE':
+        query = client.query(kind=constants.boats)
+        results = list(query.fetch())
+        if check_ID_existence(results, id) == False:
+            return(jsonErrorStringBoatID, 404)
+        boat_key = client.key(constants.boats, int(id))
+        client.delete(boat_key) #delete the boat
+        return ('', 204)
+    
+    elif request.method == 'PUT':
+        content = request.get_json()
+        if (not(content)): #if could not get the content in json format
+            return(jsonOnlyAcceptJson, 415)
+        if  "id" in content:
+            return (jsonInvalidRequest, 400)
+        if "name" not in content or "type" not in content or "length" not in content:
+            return (jsonErrorStringArgs, 400)
+        query = client.query(kind=constants.boats)
+        results = list(query.fetch()) #load boats into list object
+        if check_ID_existence(results, id) == False:
+            return(jsonErrorStringBoatID, 404)
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key) #load the boat
+        boat.update({"name": content["name"], "type": content["type"],
+          "length": content["length"]})
+        client.put(boat)
+        boat['id'] = int(id)
+        create_self_second(boat)
+        return (boat, 303)
+    
+    elif request.method == 'PATCH':
+        content = request.get_json()
+        if (not(content)): #if could not get the content in json format
+            return(jsonOnlyAcceptJson, 415)
+        if  "id" in content:
+            return (jsonInvalidRequest, 400)
+        if "name" not in content and "type" not in content and "length" not in content:
+            return (jsonErrorStringArgs, 400)
+        query = client.query(kind=constants.boats)
+        results = list(query.fetch()) #load boats into list object
+        if check_ID_existence(results, id) == False:
+            return(jsonErrorStringBoatID, 404)
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key) #load the boat
+        if "name" in content:
+            boat.update({"name": content["name"]})
+        if "length" in content:
+            boat.update({"length": content["length"]})
+        if "type" in content:
+            boat.update({"type": content["type"]})
+        client.put(boat)
+        boat['id'] = int(id)
+        create_self_second(boat)
+        return (boat, 200)
+
+    elif request.method == 'GET':
+        query = client.query(kind=constants.boats)
+        results = list(query.fetch()) #load boats into list object          
+        if check_ID_existence(results, id) == False:
+            return(jsonErrorStringBoatID, 404)
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key) #load the boat
+        boat['id'] = int(id)
+        if 'application/json' in request.accept_mimetypes:
+            jsonData = json.dumps(boat)
+            return (jsonData, 200)
+        elif 'text/html' in request.accept_mimetypes:
+            res = make_response(json2html.convert(json = json.dumps(boat)))
+            res.headers.set('Content-Type', 'text/html')
+            res.status_code = 200
+            return res
+        else:
+            return (jsonTypeNotSupported, 406)
+
+    else:
+        return 'Method not recogonized'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
