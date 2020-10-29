a9222840068b1fe85c600fe0297f0c7af01740bb
DavisHenckel <davishenckel@hotmail.com>
2020-10-28 18:20:41 -0700

    Added support for status code 415 in POST

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.cloud import datastore
-from flask import Flask, request
+from flask import Flask, request, make_response
 import json
+from json2html import *
 import constants
 
 app = Flask(__name__)
@@ -26,16 +27,28 @@ def create_self(item):
 def boats_get_or_post():
     if request.method == 'POST':
         content = request.get_json()
+        if (not(content)):
+            return('This method only accepts JSON', 415)
         if "name" not in content or "type" not in content or "length" not in content:
             return (jsonErrorStringArgs, 400)
         new_boat = datastore.entity.Entity(key=client.key(constants.boats))
         new_boat.update({"name": content["name"], "type": content["type"],
-          "length": content["length"],"loads": content["loads"]})
+          "length": content["length"]})
         client.put(new_boat)
         new_boat["id"] = new_boat.id
         create_self(new_boat)
         return (new_boat, 201)
+    
+    elif request.method == 'GET':
+        query = client.query(kind=constants.boats)
+        results = list(query.fetch())
+        res = make_response(json2html.convert(json = json.dumps(results)))
+        res.headers.set('Content-Type', 'text/html')
+        res.status_code = 200
+        return res
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    
+    
